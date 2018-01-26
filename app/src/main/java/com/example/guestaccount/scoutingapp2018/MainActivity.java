d019c651fa2db4db654f9eb6f56e51e42aa61a8c
themultininja <elijahgabrieljope@gmail.com>
2018-01-25 20:44:12 -0500

    Variable attempts

--- app/src/main/java/com/example/guestaccount/scoutingapp2018/MainActivity.java
+++ app/src/main/java/com/example/guestaccount/scoutingapp2018/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.guestaccount.scoutingapp2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.guestaccount.scoutingapp2018.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     /** Called when the use taps the Send button */
-    public void sendMessage(View view) {
-        //Do something in response to button
+    // public void sendMessage(View view) {
+    //Do something in response to button
+    //    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    //    EditText editText =(EditText) findViewById(R.id.editText);
+    //    String message =editText.getText().toString();
+    //    intent.putExtra(EXTRA_MESSAGE, message);
+    //    startActivity(intent);
+    //}
 
+    public void increaseScale (View view) {
+        TextView Scale_Display = findViewById(R.id.Scale_display);
+        int Scale = Integer.parseInt(Scale_Display.getText().toString());
+        Scale_Display.setText(""+Scale++);
     }
+
 }
