fd4463335b8f2b7f7c070f6ba602e20c56c24193
themultininja <elijahgabrieljope@gmail.com>
2018-02-01 20:56:47 -0500

    Created Initial Page

--- app/src/main/java/com/example/guestaccount/scoutingapp2018/MainActivity.java
+++ app/src/main/java/com/example/guestaccount/scoutingapp2018/MainActivity.java
@@ -8,26 +8,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.guestaccount.scoutingapp2018.MESSAGE";
+    //public static final String EXTRA_MESSAGE = "com.example.guestaccount.scoutingapp2018.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /** Called when the use taps the Send button */
-    // public void sendMessage(View view) {
-    //Do something in response to button
-    //    Intent intent = new Intent(this, DisplayMessageActivity.class);
-    //    EditText editText =(EditText) findViewById(R.id.editText);
-    //    String message =editText.getText().toString();
-    //    intent.putExtra(EXTRA_MESSAGE, message);
-    //    startActivity(intent);
-    //}
 
-    public void increaseScale (View view) {
-        TextView Scale_Display = findViewById(R.id.Scale_display);
-        int Scale = Integer.parseInt(Scale_Display.getText().toString());
-        Scale_Display.setText(""+Scale++);
+    /**
+     * Called when the use taps the Start button
+     */
+    public void startMatch(View view) {
+        //Do something in response to button
+            Intent intent = new Intent(this, Autonomous.class);
+        //    EditText editText =(EditText) findViewById(R.id.editText);
+        //    String message =editText.getText().toString();
+        //    intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        //}
+
+        //public void increaseScale (View view) {
+        //  TextView Scale_Display = findViewById(R.id.Scale_display);
+        //  int Scale = Integer.parseInt(Scale_Display.getText().toString());
+        //  Scale_Display.setText(""+Scale++);
     }
-
 }
