2c90032d2ee105f3a0500237c74abdca5c368263
themultininja <elijahgabrieljope@gmail.com>
2019-01-12 09:48:26 -0500

    Button colors investigated, variables added

--- app/src/main/java/com/example/guestaccount/scoutingapp2018/Autonomous.java
+++ app/src/main/java/com/example/guestaccount/scoutingapp2018/Autonomous.java
@@ -8,23 +8,47 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Autonomous extends AppCompatActivity {
+
+public class Autonomous extends AppCompatActivity implements OnClickListener {
     Button scale_auto_up;
     Button scale_auto_down;
-    private int i;
+    Button switch_auto_up;
+    Button switch_auto_down;
+    Button deliver_auto_up;
+    Button deliver_auto_down;
+    TextView scale_auto_entry;
+    TextView switch_auto_entry;
+    TextView deliver_auto_entry;
+    public static int scale_auto=0;
+    public static int switch_auto=0;
+    public static int deliver_auto=0;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autonomous);
 
-        scale_auto_up = (Button)findViewById(R.id.scale_auto_up);
-        scale_auto_down = (Button)findViewById(R.id.scale_auto_down);
+        scale_auto_up = findViewById(R.id.scale_auto_up);
+        scale_auto_down = findViewById(R.id.scale_auto_down);
+        switch_auto_up = findViewById(R.id.switch_auto_up);
+        switch_auto_down = findViewById(R.id.switch_auto_down);
+        deliver_auto_up = findViewById(R.id.deliver_auto_up);
+        deliver_auto_down = findViewById(R.id.deliver_auto_down);
+        scale_auto_entry = findViewById(R.id.scale_auto_entry);
+        switch_auto_entry = findViewById(R.id.switch_tele_entry);
+        deliver_auto_entry = findViewById(R.id.deliver_auto_entry);
 
-        scale_auto_up.setOnClickListener((OnClickListener) this);
-        scale_auto_down.setOnClickListener((OnClickListener) this);
-
-        i=0;
-
+        scale_auto_up.setOnClickListener(this);
+        scale_auto_down.setOnClickListener(this);
+        switch_auto_up.setOnClickListener(this);
+        switch_auto_down.setOnClickListener(this);
+        deliver_auto_up.setOnClickListener(this);
+        deliver_auto_down.setOnClickListener(this);
+        scale_auto_entry.setText(String.valueOf(scale_auto));
+        switch_auto_entry.setText(String.valueOf(switch_auto));
+        deliver_auto_entry.setText(String.valueOf(deliver_auto));
 /*
         Button scale_auto_up = (Button) findViewById(R.id.scale_auto_up);
         final TextView scale_auto_entry = (TextView) findViewById(R.id.scale_auto_entry);
@@ -109,7 +133,7 @@ public class Autonomous extends AppCompatActivity {
 
         ); */
 }
- public void scale_auto(View v) {
+ /*public void scale_auto(View v) {
         if(v==scale_auto_up){
             i++;
         }  else if(v==scale_auto_down){
@@ -118,7 +142,7 @@ public class Autonomous extends AppCompatActivity {
                 i=0;
             }
         }
- }
+ }*/
 
     public void Teleop (View view) {
         Intent tele = new Intent(this, Tele.class);
@@ -130,4 +154,37 @@ public class Autonomous extends AppCompatActivity {
         startActivity(notes);
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v==scale_auto_up){
+            scale_auto++;
+        }
+        else if(v==scale_auto_down){
+            scale_auto--;
+            if (scale_auto<0){
+                scale_auto=0;
+            }
+        }
+        else if(v==switch_auto_up) {
+            switch_auto++;
+        }
+        else if(v==switch_auto_down){
+            switch_auto--;
+            if(switch_auto<0){
+                switch_auto=0;
+            }
+        }
+        else if(v==deliver_auto_up){
+            deliver_auto++;
+        }
+        else if(v==deliver_auto_down){
+            deliver_auto--;
+            if(deliver_auto<0){
+                deliver_auto=0;
+            }
+        }
+        scale_auto_entry.setText(String.valueOf(scale_auto));
+        switch_auto_entry.setText(String.valueOf(switch_auto));
+        deliver_auto_entry.setText(String.valueOf(deliver_auto));
+    }
 }
